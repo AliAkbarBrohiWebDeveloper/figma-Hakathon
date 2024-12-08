e67c9a0f5174e2744363d518eaa34f74ca89ac90
Ali Akbar Brohi Web Developer <aliakberbrohi455@gmail.com>
2024-12-08 13:56:49 +0500

    commit changes

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -18,7 +18,7 @@ const Cart  = () => {
 
 <h3 className='text-xl font-bold'>Nike Air Force 1 
 PLT.AF.ORM</h3>
-<p>Turn style on its head with this crafted take on the Air Jordan 1 Mid. Its "inside out"-inspired construction, including unique layering and exposed foam accents, ups the ante on this timeless Jordan Brand silhouette. Details like the deco stitching on the Swoosh add coveted appeal, while the unexpected shading, rich mixture of materials and aged midsole aesthetic give this release an artisan finish.</p>
+<p>Turn style on its head with this crafted take on the Air Jordan 1 Mid. Its inside out-inspired construction, including unique layering and exposed foam accents, ups the ante on this timeless Jordan Brand silhouette. Details like the deco stitching on the Swoosh add coveted appeal, while the unexpected shading, rich mixture of materials and aged midsole aesthetic give this release an artisan finish.</p>
 <span className='text-xl font-bold'>₹ 8 695.00</span>
 <br />
 <button className='bg-black px-6 py-2 rounded-lg  text-white mt-6'>  Add To Cart</button>
